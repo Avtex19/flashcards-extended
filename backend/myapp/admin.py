e1f2b538b32aa9fffb69023c31286352a60dcaa7
Avtex19 <atukaberadze19@gmail.com>
2025-05-05 16:54:17 +0400

    Customize admin for CustomUser and register Deck, Flashcard, Category

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -1,3 +1,22 @@
+# myapp/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import *
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('birth_date',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('birth_date',)}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Deck)
+admin.site.register(Flashcard)
+admin.site.register(Category)
+
