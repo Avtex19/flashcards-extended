34506205d850269f6bfdfa317a0e8fa8316f6361
Avtex19 <atukaberadze19@gmail.com>
2025-05-07 12:32:58 +0400

    Refactored viewsets to integrate DjangoFilterBackend for filtering.
    - Replaced custom `get_queryset` methods with DjangoFilterBackend and filters.
    - Added `FlashcardFilter` and `DeckFilter` for filtering Flashcards and Decks by category and deck.
    - Changed viewsets to `ReadOnlyModelViewSet` for all models.

--- backend/myapp/api/urls.py
+++ backend/myapp/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from ..api.views import FlashCardViewSet, DeckViewSet, CategoryViewSet, HintViewSet
+from ..api.views import FlashcardViewSet, DeckViewSet, CategoryViewSet, HintViewSet
 
 router = DefaultRouter()
-router.register('flashcards', FlashCardViewSet,'flashcard')
+router.register('flashcards', FlashcardViewSet, 'flashcard')
 router.register('decks', DeckViewSet)
 router.register('categories', CategoryViewSet)
 router.register('hints', HintViewSet)
--- backend/myapp/api/views.py
+++ backend/myapp/api/views.py
@@ -1,38 +1,30 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
+from .filters import FlashcardFilter, DeckFilter
 from .serializers import FlashcardSerializer, CategorySerializer, DeckSerializer, HintSerializer
 from ..models import Flashcard, Category, Deck, Hint
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class DeckViewSet(viewsets.ModelViewSet):
-    serializer_class = DeckSerializer
+class DeckViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Deck.objects.all()
-
-    def get_queryset(self):
-        category_id = self.request.query_params.get('category')
-        if category_id:
-            return Deck.objects.filter(category_id=category_id)
-        return Deck.objects.all()
+    serializer_class = DeckSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = DeckFilter
 
 
-class FlashCardViewSet(viewsets.ModelViewSet):
+class FlashcardViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Flashcard.objects.all()
     serializer_class = FlashcardSerializer
-
-    def get_queryset(self):
-        deck_id = self.request.query_params.get('deck')
-        category_id = self.request.query_params.get('category')
-        if deck_id:
-            queryset = Flashcard.objects.filter(deck_id=deck_id)
-        if category_id:
-            queryset = Flashcard.objects.filter(deck__category_id=category_id)
-        return queryset
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = FlashcardFilter
 
 
-class HintViewSet(viewsets.ModelViewSet):
+class HintViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Hint.objects.all()
     serializer_class = HintSerializer
