cd854620c087549d32628aa622b2a84bf52a6246
Avtex19 <atukaberadze19@gmail.com>
2025-05-12 16:05:58 +0400

    Add register path in urls

--- backend/myapp/auth/urls.py
+++ backend/myapp/auth/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView, TokenBlacklistView
 
+from .views import UserRegistrationView
+
 urlpatterns = [
     path('', views.getRoutes),
+    path('register', UserRegistrationView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
